a0376d7a7745d3c213c5395fe9f76d5fbbc10809
bubibubi <u.ballestrazzi@gmail.com>
2017-02-27 19:31:52 +0100

    Add project files.

---
+++ XamarinXaml/XamarinXaml/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinXaml
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Somma_Clicked(object sender, EventArgs e)
+        {
+            int iPrimoNumero;
+            int iSecondoNumero;
+
+            if (!int.TryParse(primoNumero.Text, out iPrimoNumero))
+            {
+                totale.Text = "(n/a)";
+                return;
+            }
+
+            if (!int.TryParse(secondoNumero.Text, out iSecondoNumero))
+            {
+                totale.Text = "(n/a)";
+                return;
+            }
+
+            totale.Text = string.Format("{0}", iPrimoNumero + iSecondoNumero);
+        }
+    }
+}
---
+++ XamarinXaml/XamarinXaml/ScrollViewSample.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamarinXaml
+{
+    public partial class ScrollViewSample : ContentPage
+    {
+        public ScrollViewSample()
+        {
+            InitializeComponent();
+            MyFamilyImage.Source = ImageSource.FromResource("XamarinXaml.MyFamily.jpg");
+        }
+    }
+}
